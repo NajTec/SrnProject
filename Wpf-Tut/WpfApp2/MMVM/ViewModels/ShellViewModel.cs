d24fdb63f3374b1fda8a360c776cf109b71b75d0
NajTec <57097612+NajTec@users.noreply.github.com>
2021-07-28 21:25:25 +0200

    Tut fast abgeschlossen

--- Wpf-Tut/WpfApp2/MMVM/ViewModels/ShellViewModel.cs
+++ Wpf-Tut/WpfApp2/MMVM/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using MMVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,16 @@ namespace MMVM.ViewModels
     public class ShellViewModel : Screen
     {
         private string _firstname = "Jan";
+        private string _lastName;
+        private Person _selectedPerson;
+        private BindableCollection<Person> _people = new BindableCollection<Person>();
 
+        public ShellViewModel()
+        {
+            People.Add(new Person { FirstName = "Jan", LastName = "Klotter" });
+            People.Add(new Person { FirstName = "Susi", LastName = "Sinnlos" });
+            People.Add(new Person { FirstName = "Max", LastName = "Max Mustermann" });
+        }
         public string FirstName
         {
             get 
@@ -19,9 +29,56 @@ namespace MMVM.ViewModels
             }
             set 
             {
-                _firstname = value; 
+                _firstname = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
             }
         }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+        public string FullName
+        {
+            get 
+            { 
+                return $"{ FirstName } { LastName }"; 
+            }
+        }
+        public BindableCollection<Person> People 
+        {
+            get { return _people; }
+            set { _people = value; } 
+        }
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set 
+            { 
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanClearText(string firstname, string lastname) {
+            return !String.IsNullOrWhiteSpace(firstname) || !String.IsNullOrWhiteSpace(lastname);
+        }
+        
+        public void ClearText(string firstname, string lastname)
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
 
     }
 }
